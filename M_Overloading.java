99d5b402458ff4c1dceb11076c4f29c60936023f
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-01 23:33:58 +0530

    done

---
+++ M_Overloading.java
@@ -0,0 +1,14 @@
+public class M_Overloading {
+
+    void sum()
+    {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args)
+    {
+        M_Overloading sample = new M_Overloading();
+        sample.sum();
+    }
+    
+}
