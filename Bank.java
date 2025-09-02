ec791f67e4be6d052806e588750eac96194c5f7d
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-02 08:28:51 +0530

    Done

---
+++ Bank.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the amount:");
+        int age = sc.nextInt();
+        System.out.print("The entered number is :"+age);
+
+    }
+}
