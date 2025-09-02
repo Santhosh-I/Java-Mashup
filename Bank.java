b29ee9a1c99a013ccab94ee5c5270760ca8f05f7
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-09-02 08:43:46 +0530

    done

--- Bank.java
+++ Bank.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 
 public class Bank {
+
+    int money = 1000;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        Bank B1 = new Bank();
         System.out.print("Enter the amount:");
-        int age = sc.nextInt();
-        System.out.print("The entered number is :"+age);
+        int amount = sc.nextInt();
+        if (B1.money>amount){
+            System.out.print("The entered amount is :"+amount);
+        }
+        else{
+            System.out.print("Insufficient Balance");
+        }
 
     }
 }
