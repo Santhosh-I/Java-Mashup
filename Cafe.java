ec6271ee5408abc34613632c6ba3c17df980b287
Santhosh-I <santhoshilaiyaraja2006@gmail.com>
2025-11-18 15:31:40 +0530

    java exam practices

---
+++ Cafe.java
@@ -0,0 +1,15 @@
+class Cafe{
+
+    int coffee = 20;
+    int tea = 15;
+    public static void main(String[] args)
+    {
+
+        Cafe s1 = new Cafe();
+        System.out.println(s1.coffee);
+
+        Cafe s2 = new Cafe();
+        System.out.println(s2.tea);
+
+    }
+}
